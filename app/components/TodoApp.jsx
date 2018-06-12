41986b3bc962e517dfcc90e30e4af4b1755a24eb
Josiah Maina <josiah5115@gmail.com>
2018-06-12 00:46:53 -0600

    to-do-app file setup

---
+++ app/components/TodoApp.jsx
@@ -0,0 +1,13 @@
+var React = require('redux');
+
+var TodoApp = React.createClass({
+  render: function() {
+    return (
+      <div>
+        TodoApp.jsx
+      </div>
+    )
+  }
+});
+
+module.exports = TodoApp;
