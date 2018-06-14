96b26bc4bb351fac29966e4764c49bc56cfb2fda
Josiah Maina <josiah5115@gmail.com>
2018-06-14 04:35:05 -0600

    create new instance of moment

--- playground/moment-examples.js
+++ playground/moment-examples.js
@@ -1,4 +1,21 @@
 var moment = require('moment');
 
 console.log(moment());
+// unix time stamps
+// January 1st 1970 @ 12.00am -> 0;
+// January 1st 170 @ 12.00am -> -60;
 
+var now = moment();
+
+console.log('Current timestamp', now.unix());
+
+var timestamp = 1528970602;
+
+var currentMoment = moment.unix(timestamp);
+
+console.log('current moment one =>', currentMoment.format('MMM D, YY @ h:mm a'));
+
+// define how you want to display/ define time(http://momentjs.com/docs/#/displaying);
+
+//  January 3rd, 2018 @ 12:13 AM;
+console.log('current moment two ==>', currentMoment.format('MMMM Qo, YYYY @ HH:mm A'));
