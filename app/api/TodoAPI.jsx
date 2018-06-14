c18685e0d989338dc6f1049d652fb3ec3caf5eb2
Josiah Maina <josiah5115@gmail.com>
2018-06-14 00:14:17 -0600

    minor edits

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -1,12 +1,16 @@
 var $ = require('jquery');
 
 module.exports = {
+  // This stores the values we enter on the browser locally.
+  // when you refresh the browser your input dosen't erase.
 	setTodos: function(todos) {
 		if ($.isArray(todos)) {
 			localStorage.setItem('todos', JSON.stringify(todos));
 			return todos;
 		}
-	},
+  },
+  
+  // This captures/ fetches the input values client types in the browser.
 	getTodos: function() {
 		var stringTodos = localStorage.getItem('todos');
 		var todos = [];
