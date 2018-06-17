8314db2e00cee0e5d095266377f0e9c5a4be1352
Josiah Maina <josiah5115@gmail.com>
2018-06-17 06:09:00 -0600

    minor edits\bug fix

--- app/app.jsx
+++ app/app.jsx
@@ -1,6 +1,6 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+var { Route, Router, IndexRoute, hashHistory } = require('react-router');
 
 var TodoApp = require('TodoApp');
 
@@ -8,9 +8,6 @@ var TodoApp = require('TodoApp');
 $(document).foundation();
 
 // App css
-require('style!css!sass!applicationStyles')
+require('style!css!sass!applicationStyles');
 
-ReactDOM.render(
-  <TodoApp/>,
-  document.getElementById('app')
-);
+ReactDOM.render(<TodoApp />, document.getElementById('app'));
