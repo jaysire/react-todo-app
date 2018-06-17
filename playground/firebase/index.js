ad87d7f444f2b219e5a0a046e300d6bb8806144f
Josiah Maina <josiah5115@gmail.com>
2018-06-16 21:40:56 -0600

    firebase installation n examples

---
+++ playground/firebase/index.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase';
+
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCkkIXzv-8GBI03EUXIYKhi6xzygwrXMws",
+    authDomain: "my-todo-app-2fa5f.firebaseapp.com",
+    databaseURL: "https://my-todo-app-2fa5f.firebaseio.com",
+    projectId: "my-todo-app-2fa5f",
+    storageBucket: "my-todo-app-2fa5f.appspot.com",
+    messagingSenderId: "270434056565"
+  };
+  firebase.initializeApp(config);
+  
