b46c50b6c28c2d60549ce9ec3034889785eab5cc
Josiah Maina <josiah5115@gmail.com>
2018-06-23 23:46:03 -0600

    firebase file

--- playground/firebase/index.js
+++ playground/firebase/index.js
@@ -1,16 +1,33 @@
-import firebase from 'firebase'
+import firebase from firebase
 
-// Initialize Firebase
-var config = {
-  apiKey: 'AIzaSyCkkIXzv-8GBI03EUXIYKhi6xzygwrXMws',
-  authDomain: 'my-todo-app-2fa5f.firebaseapp.com',
-  databaseURL: 'https://my-todo-app-2fa5f.firebaseio.com',
-  projectId: 'my-todo-app-2fa5f',
-  storageBucket: 'my-todo-app-2fa5f.appspot.com',
-  messagingSenderId: '270434056565'
-}
-firebase.initializeApp(config)
 
-firebase.database().ref.apply().set({
-  appName: 'Todo App'
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import firebase from 'firebase'
+
+// // Initialize Firebase
+// var config = {
+//   apiKey: 'AIzaSyCkkIXzv-8GBI03EUXIYKhi6xzygwrXMws',
+//   authDomain: 'my-todo-app-2fa5f.firebaseapp.com',
+//   databaseURL: 'https://my-todo-app-2fa5f.firebaseio.com',
+//   projectId: 'my-todo-app-2fa5f',
+//   storageBucket: 'my-todo-app-2fa5f.appspot.com',
+//   messagingSenderId: '270434056565'
+// }
+// firebase.initializeApp(config)
+
+// firebase.database().ref.apply().set({
+//   appName: 'Todo App'
+// })
